6fe148ada63d4ca587548d256e477b067760faf6
franklinlugo <franklinlugo2805@gmail.com>
2020-10-12 18:47:57 -0300

    install basic apps script

---
+++ debian-after-install/install-basics-apps.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+apt update && apt upgrade && apt dist-upgrade
+
+apt install --yes \
+gimp \
+gimp-data \
+gimp-plugin-registry \
+gimp-data-extras \
+vlc \
+rar \
+unrar \
+zip \
+unzip \
+deluge \
+guake \
+zsh \
+git \
+gnome-shell-pomodoro
+
+
+# skype dropbox atom
