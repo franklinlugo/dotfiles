307663fe1486a95f454a3c0423885d9be6a231c3
franklinlugo <franklinlugo2805@gmail.com>
2020-10-12 20:05:13 -0300

    bash aliases

--- backup-before-format.sh
+++ backup-before-format.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rsync -av --delete /root/.bash_aliases /home/franklin/repositories/dotfiles/bash_aliases-root
+rsync -av --delete /root/.bash_aliases /home/franklin.lugo/repositories/dotfiles/bash_aliases-root
 rsync -av --delete /home/franklin.lugo/.bash_aliases /home/franklin.lugo/repositories/dotfiles/bash_aliases-user
 rsync -av --delete /root/.zshrc /home/franklin.lugo/repositories/dotfiles/.zshrc-root
 rsync -av --delete /home/franklin.lugo/.zshrc /home/franklin.lugo/repositories/dotfiles/.zshrc-user
